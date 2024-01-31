f5ceacba7d9a4bec682a14b96fb0624a83a86831
Gonzalo Orsi <gonzaloorsi@gmail.com>
2024-01-30 22:07:24 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    document.title = `Hello! ${count}`;
+  }, [count]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World!</h1>
+        <p>Start counting: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Add 1</button>
       </header>
     </div>
   );
