720fc0f1101b784b30e59b9aca56acc4f3eeae7a
Gonzalo Orsi <gonzaloorsi@gmail.com>
2024-01-30 23:12:05 -0300

    Update page title dynamically post-form submission

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = React.useState(0);
+  const [name, setName] = React.useState("");
+  const [titleName, setTitleName] = React.useState("");
+
   React.useEffect(() => {
-    document.title = `Hello! ${count}`;
-  }, [count]);
+    document.title = `Hello, ${titleName}! | ${count}`;
+  }, [count, titleName]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            setTitleName(name);
+            setName("");
+          }}
+        >
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+        {titleName && <p>Your name is: {titleName}</p>}
         <p>Start counting: {count}</p>
         <button onClick={() => setCount(count + 1)}>Add 1</button>
       </header>
